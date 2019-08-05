990dc48993e67535d82432ef75e825b7d84e7ba1
dangtrungdev <dangtrungdev113999@gmail.com>
2019-08-05 20:58:37 +0700

    [1.0.0] register user part 01

---
+++ src/validation/authValidation.js
@@ -0,0 +1,21 @@
+import {check} from 'express-validator/check';
+import {tranValidation} from './../../lang/vi'; 
+
+let register = [
+  check("email", tranValidation.email_incorrect)
+    .isEmail()
+    .trim(),
+  check("gender", tranValidation.gender_incorrect)
+    .isIn(["male", "female"]),
+  check("password", tranValidation.password_incorrect)
+    .isLength({min: 8})
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/),
+  check("password_confirmation", tranValidation.password_confirmation_incorrect)
+    .custom((value, {req}) => {
+      return value === req.body.password;
+    })
+];
+
+module.exports = {
+  register
+}
