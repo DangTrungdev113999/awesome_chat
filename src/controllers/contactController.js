4cd55f1f0c90730362c219703643cc433e82f942
dangtrungdev <dangtrungdev113999@gmail.com>
2019-08-10 18:20:26 +0700

    [1.0.0} Add & remove Contact

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -13,19 +13,45 @@ let findUsersContact = async (req, res) => {
     return res.status(500).send(errorArr);
   };
 
-
   try {
     let currentUserId = req.user._id;
     let keyword = req.params.keyword;
 
     let users = await contact.findUsersContact(currentUserId, keyword);
-    console.log(users);
     return res.render("main/contact/sessions/_findUserscontact", {users});
   } catch (error) {
     return res.status(500).send(error);
   };
 };
 
+let addNew = async(req, res) => {
+  try {
+
+    let currentUserId = req.user._id;
+    let contactId = req.body.uid;
+    let newContact = await contact.addNew(currentUserId, contactId);
+    return res.status(200).send({success: !!newContact});
+
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+let removeRequestContact = async (req, res) => {
+  try {
+
+    let currentUserId = req.user._id;
+    let contactId = req.body.uid;
+    let removeReq = await contact.removeRequestContact(currentUserId, contactId);
+    return res.status(200).send({success: !!removeReq});
+
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+}
+
 module.exports = {
-  findUsersContact
+  findUsersContact,
+  addNew,
+  removeRequestContact
 };
